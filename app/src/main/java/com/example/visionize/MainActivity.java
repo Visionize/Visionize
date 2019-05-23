b4fc41acf19f1ae3ce4e8c5cf5e72f73a2eca747
Nishkrit Desai <nishkritdesai10@gmail.com>
2019-05-23 12:57:34 -0400

    Updated UI to and wrote some boilerplate java

--- app/src/main/java/com/example/visionize/MainActivity.java
+++ app/src/main/java/com/example/visionize/MainActivity.java
@@ -1,9 +1,23 @@
 package com.example.visionize;
 
+import android.media.ImageReader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.imageclassifier.ImageClassifier;
+
+public abstract class MainActivity extends AppCompatActivity
+        implements ImageReader.OnImageAvailableListener, View.OnClickListener, AdapterView.OnItemSelectedListener {
+
+    private static final String MODEL_PATH = "mobilenet_v2_1.4_224.tflite";
+    private static final String LABEL_PATH = "labels.txt";
+    private static final boolean QUANT_MODEL = true;
+    private static final int INPUT_SIZE = 224;
+
+    ImageClassifier classifier;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
